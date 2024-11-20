e2b9145e53951a16075137013f56e6027f08f895
sangpham2006 <pty49990@gmail.com>
2024-11-21 00:18:30 +0700

    session2-lession6

---
+++ bai 6.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(){
+	const float PI = 3.14;
+	int banKinh = 6;
+	float chuVi = 2 * PI * banKinh;
+	printf("%.2f \n", chuVi);	  
+	float dienTich = PI * banKinh * banKinh;
+	printf("%.2f \n", dienTich);	  
+
+	
+	
+	return 0;
+} 
