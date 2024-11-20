c099e24ad85b2150f6245cf64e6cc6179f4859c7
sangpham2006 <pty49990@gmail.com>
2024-11-21 00:16:09 +0700

    nộp bài 1

---
+++ bai 1 (1).c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(){
+	 // kieu du lieu so nguyen 
+	 int sang_pham_k6;
+	 // kieu du lieu so thuc
+	 float tron_lang;
+	 double ka_ta;
+	 // kieu du lieu ki tu 
+	 char son_zet;
+
+	return 0;
+} 
